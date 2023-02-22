3888053c62549ee3c374b88c08db388048c03f59
riazahmed01 <00.riaz.ahmed@gmail.com>
2023-02-22 00:30:26 -0500

    added some html files

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, redirect, url_for, Markup
+# import libraries
+from flask import Flask, render_template, Markup
+
+# create flask app
 app = Flask(__name__)
 
+# route home/index page
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def index():
+    return render_template("index.html")
+
+# route login page
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+# route user page
+@app.route("/user/")
+def user():
+    return render_template("user.html")
