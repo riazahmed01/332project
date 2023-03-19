4ab6462d74f9b4004b6acb862d87d203b76a1157
riazahmed01 <00.riaz.ahmed@gmail.com>
2023-03-19 06:21:36 -0400

    test

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# route custom builds page
+@app.route("/custombuilds/")
+def custombuild():
+    return render_template("custombuild.html")
+
+# route cpu page
+@app.route("/cpu/")
+def cpu():
+    return render_template("cpu.html")
+
+# route cart page
+@app.route("/cart/")
+def cart():
+    return render_template("cart.html")
+
 # route login page
 @app.route("/login/")
 def login():
@@ -18,3 +33,7 @@ def login():
 @app.route("/user/")
 def user():
     return render_template("user.html")
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
