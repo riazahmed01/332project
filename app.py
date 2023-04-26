2cd18d5ece76d06ddd2d767872b28110db4e8692
JNikolo <125705821+JNikolo@users.noreply.github.com>
2023-04-25 23:50:45 -0400

    db created

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 # import libraries
-from flask import Flask, render_template, Markup
-
+from flask import Flask, redirect, render_template, Markup, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime 
 # create flask app
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/ComputerStore'
+
+db = SQLAlchemy(app)
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(300), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return '<Name %r>' %self.id
+registered_users = dict()
+  
 # route home/index page
 @app.route("/")
 def index():
@@ -25,20 +38,51 @@ def cart():
     return render_template("cart.html")
 
 # route login page
-@app.route("/login/")
+@app.route("/login/", methods=['POST','GET'])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        email_in = request.form['email']
+        password_in = request.form['password']
+        if (email_in in registered_users.keys()) and (registered_users[email_in] == password_in):
+            username = email_in
+            return render_template("index.html")
+        else:
+            return "Log In failed. You are not registered."
+    else:
+        return render_template("login.html")
 
 # route signup page
-@app.route("/signup/")
+
+@app.route("/signup/",methods=['POST','GET'])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+        rg_email = request.form['email']
+        rg_password = request.form['password']
+        
+        if rg_email not in registered_users.keys():
+            registered_users[rg_email] = rg_password
+            return redirect('/login')
+        else:
+            return "Email already registered"
+    else:
+        return render_template("signup.html")
 
 # route product page
-@app.route("/product/")
+@app.route("/product/", methods=['POST','GET'])
 def product():
-    return render_template("product.html")
+    if request.method == "POST":
+        guest_text = request.form['comment']
+        new_text = Comments(text=guest_text)
 
+        try:
+            db.session.add(new_text)
+            db.session.commit()
+            return redirect('/product')
+        except:
+            return "Comment was longer than 300 characters"
+    else:
+        texts = Comments.query.order_by(Comments.date_created)
+        return render_template("product.html", texts = texts)
 # route user page
 @app.route("/user/")
 def user():
