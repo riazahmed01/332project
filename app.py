1d9a8e42e6c306dc271ec6210296074e914f9d2b
MKhan104 <mkhan042@citymail.cuny.edu>
2023-05-16 00:01:05 -0400

    Minor bugs fixed

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ class PaymentMethod(db.Model):
     card_number = db.Column(db.String(16), nullable=False)
     card_type = db.Column(db.String(20), nullable=False)
     expiration_date = db.Column(db.String(7), nullable=False)
-    cvv = db.Column(db.String(3), nullable=False)
+    cvv = db.Column(db.String(4), nullable=False)
 
     def __repr__(self):
         return f'<PaymentMethod {self.card_type} {self.card_number}>'
